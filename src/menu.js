ec5b187533e81d03f493130303a5ef4846759723
Devkass7 <kmuniru7@gmail.com>
2026-08-15 23:17:02 +0000

    designed the menu page yet to style

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,8 @@
-
+import logo from "./images/logo.png"
+import jollof from "./images/jollof.jpg"
+import fries from "./images/fries.jpg"
+import ayoyo from "./images/ayoyo.jpg"
+import pizza from "./images/pizza.jpg"
 
 
  function menu(){
@@ -7,8 +11,46 @@
 
     const menuItems = document.createElement("div")
 
-    menuItems.innerHTML = '<div><img src="./images/logo.png" alt="logo"><h3>Jollof Rice</h3></div><div><img src="./images/logo.png" alt="logo"><h3>Pizza</h3></div><div><img src="./images/logo.png" alt="logo"><h3>Banku</h3></div><div><img src="./images/logo.png" alt="logo"><h3>Fries</h3></div>'
+      
+    menuItems.innerHTML = `<div>
+    <h1>Your Meal is Just a <span>Click</span> away</h1>
+</div>
+<fieldset>
+    <legend>
+        <img class="logo" src="${logo}" alt="logo" />
+    </legend>
+    <div>
+        <h3>Assorted Jollof Rice</h3>
+        <img class="food-image" src="${jollof}" alt="" /><button class="cta-button">Place Order</button>
+    </div>
+</fieldset>
 
+<fieldset>
+    <legend><img class="logo" src="${logo}" alt="logo" /></legend>
+    <div>
+        <h3>Special Banku</h3>
+        <img class="food-image" src="${ayoyo}" alt="" /><button class="cta-button">Place Order</button>
+    </div>
+</fieldset>
+
+<fieldset>
+    <legend><img class="logo" src="${logo}" alt="logo" /></legend>
+    <div>
+        <h3>Irresistible Fries</h3>
+        <img class="food-image" src="${fries}" alt="" /><button class="cta-button">Place Order</button>
+    </div>
+</fieldset>
+
+<fieldset>
+    <legend><img class="logo" src="${logo}" alt="logo" /></legend>
+    <div>
+        <h3>Amazing</h3>
+        <img class="food-image" src="${pizza}" alt="" /><button class="cta-button">Place Order</button>
+    </div>
+</fieldset>
+`
+
+   // menuItems.innerHTML = `<img scr='${logo}' >`
     menu.appendChild(menuItems)
 }
 
